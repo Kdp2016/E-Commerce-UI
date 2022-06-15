61d1048b3dc7f8f253e30d5f1f599f818054202e
dheijaj <dheijaj@gmail.com>
2022-06-15 14:27:28 -0700

    created login component

---
+++ src/components/Login.tsx
@@ -0,0 +1,38 @@
+import { SyntheticEvent } from "react";
+import { useState } from "react";
+
+function Login(props: {}) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+
+    let updateUsername = (e: SyntheticEvent) => {
+        setUsername((e.target as HTMLInputElement).value);
+    }
+
+    let updatePassword = (e: SyntheticEvent) => {
+        setPassword((e.target as HTMLInputElement).value);
+    }
+
+    let login = (e: SyntheticEvent) => {
+        if (!username || !password) {
+            setMessage("You must provide a Username and Password.");
+        } else {
+            setMessage("Login success!");
+            console.log("username: " + username);
+            console.log("password: " + password);
+        }
+    }
+    return (
+        <>
+            <h1>loginpage</h1>
+            <input type="text" onChange={updateUsername} placeholder="username"></input>
+            <input type="password" onChange={updatePassword} placeholder="password"></input>
+            <button onClick={login} id="login-button">login</button>
+            {message && <p>{message}</p>}
+
+        </>
+    );
+}
+
+export default Login;
