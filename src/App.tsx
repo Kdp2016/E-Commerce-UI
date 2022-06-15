7ba9138cba6fb6032cb1b495adfe57f4de098fa0
unknown <105248515+Vaertox@users.noreply.github.com>
2022-06-15 15:19:52 -0700

    created register component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
     <>
-
       <Login />
+      <Register />
     </>
   );
 }
