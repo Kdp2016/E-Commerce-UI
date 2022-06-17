0cced87321aeb0f8066ac8563badf67330b5a389
JoshuaMobbley <joshua534@revature.net>
2022-06-16 20:08:40 -0500

    Created Dashboard UI

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,18 @@
+import { Navigate } from "react-router-dom";
+import { UserInterface } from "../models/user";
+
+interface IDashboardProps {
+    currentUser: UserInterface | undefined
+}
+
+function Dashboard(props: IDashboardProps) {
+
+     return (
+        !props.currentUser ? <Navigate to="/login" /> :
+        <>
+            <h1>Welcome, {props.currentUser.firstName}!</h1>
+        </>
+    );
+}
+
+export default Dashboard;
