3106c1e532dff0f38e11a38a729ddf32ad64a49b
SosaTheHustler <arcane1241@gmail.com>
2022-06-21 14:06:38 -0400

    added models and updated cart css

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,12 +1,5 @@
-import {
-    Button,
-    Card,
-    CardMedia,
-    Grid,
-  } from "@mui/material";
+import { Button, Card, CardMedia, Grid } from "@mui/material";
 import { Product } from "../models/Product";
-
-
 
 type Props = {
   product: Product;
@@ -16,36 +9,26 @@ type Props = {
 const ProductCard = ({ product, handleAddToCart }: Props) => {
   return (
     <Grid item key={product.id} xs={4} md={4} lg={2.8}>
-              <Card className="product">
-                <CardMedia component="img" image={product.productImage} />
-                <h4>{product.productName}</h4>
-                <h3>${product.price} USD</h3>
-                <Button
-                  className="addToCart"
-                  size="large"
-                  variant="contained"
-                  color="primary"
-                  onMouseDown={(event) => event.stopPropagation()}
-                  onClick={() => handleAddToCart(product)}
-                >
-                  Add To Cart
-                </Button>
-              </Card>
-            </Grid>
+      <Card className="product">
+        <CardMedia component="img" image={product.productImage} />
+        <div>
+          <h4>{product.productName}</h4>
+          <h6>{product.category.category_name}</h6>
+        </div>
+        <h3>${product.price} USD</h3>
+        <Button
+          className="addToCart"
+          size="large"
+          variant="contained"
+          color="primary"
+          onMouseDown={(event) => event.stopPropagation()}
+          onClick={() => handleAddToCart(product)}
+        >
+          Add To Cart
+        </Button>
+      </Card>
+    </Grid>
   );
 };
 
 export default ProductCard;
-
-
-
-
-
-
-
-
-
-
-
-
-
