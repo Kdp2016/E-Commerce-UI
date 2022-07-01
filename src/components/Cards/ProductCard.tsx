44515a0ac2078f29b01a95a9169063d1a45b608e
SosaTheHuster <arcane1241@gmail.com>
2022-07-01 15:03:44 -0400

    product create method complete

--- src/components/Cards/ProductCard.tsx
+++ src/components/Cards/ProductCard.tsx
@@ -10,12 +10,15 @@ const ProductCard = ({ product, handleAddToCart }: Props) => {
   return (
     <Grid item key={product.id} xs={4} md={4} lg={2.8}>
       <Card className="product">
+        <div>
         <CardMedia component="img" image={product.productImage} />
         <div>
           <h4>{product.productName}</h4>
           <h6>{product.category}</h6>
-        </div>
+        </div></div>
+        <div>
         <h3>${product.price} USD</h3>
+        
         <Button
           className="addToCart"
           size="large"
@@ -25,7 +28,7 @@ const ProductCard = ({ product, handleAddToCart }: Props) => {
           onClick={() => handleAddToCart(product)}
         >
           Add To Cart
-        </Button>
+        </Button></div>
       </Card>
     </Grid>
   );
