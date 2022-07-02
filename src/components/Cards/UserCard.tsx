739b127b9fa597a20a62fb43e8e2750146cd98d2
SosaTheHuster <arcane1241@gmail.com>
2022-07-01 22:17:16 -0400

    dashboard conditional rendering and update User

--- src/components/Cards/UserCard.tsx
+++ src/components/Cards/UserCard.tsx
@@ -1,4 +1,5 @@
-import { Button, ButtonGroup, Card, CardMedia, Grid } from "@mui/material";
+import { Box, Button, ButtonGroup, Card, CardMedia, FormControl, Grid, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+import { SyntheticEvent, useState } from "react";
 import { User } from "../../models/User";
 
 type Props = {
@@ -6,6 +7,29 @@ type Props = {
 };
 
 const UserCard = ({ user }: Props) => {
+  const [role, setRole] = useState(user.role);
+  const [show, setShow] = useState(false);
+  
+  let updateRole = (e: SyntheticEvent) => {
+    setRole((e.target as HTMLInputElement).value);
+  }
+
+let updateUser = () => {
+  fetch(`http://Ecommerce-env.eba-hz3mknpp.us-east-1.elasticbeanstalk.com/ecommerce/users`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: user.id,
+      role: role,
+    }),
+  }).then((resp) => {
+    setShow(false);
+    return resp.json();
+  });
+}
+
   return (
     <Grid item key={user.id} xs={4} md={4} lg={2.8}>
       <Card className="product">
@@ -17,9 +41,29 @@ const UserCard = ({ user }: Props) => {
           <h3>{user.role}</h3>
         </div>
         <ButtonGroup>
-          <Button>Update</Button>
+          <Button onClick={() => setShow(true)}>Update</Button>
           <Button>Delete</Button>
         </ButtonGroup>
+        <div style={{ display: show ? "block" : "none" }}>
+          <Box>
+            <h1>Update Role</h1>
+            <FormControl sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="demo-simple-select-helper-label">Role</InputLabel>
+            <Select
+              labelId="demo-simple-select-helper-label"
+              id="demo-simple-select-helper"
+              value={role}
+              label="Role"
+              onChange={event => setRole(event.target.value as string)}            >
+              <MenuItem value="ADMIN">ADMIN</MenuItem>
+              <MenuItem value="BUYER">BUYER</MenuItem>
+              <MenuItem value="SELLER">SELLER</MenuItem>
+            </Select>
+          </FormControl>
+
+            <Button onClick={updateUser}>Update</Button>
+          </Box>
+        </div>
       </Card>
     </Grid>
   );
